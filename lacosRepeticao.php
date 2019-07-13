641bb14a210df8074b29e5085be01ea1d1d43b99
Administrador <administrador@gmail.com>
2019-07-12 21:38:05 -0300

    Revert "Vitor Henrique me criticou... estou magoado..."
    This reverts commit 0b4f66c41fa8b740cc8c820ecd2646ef8e221d6d.

---
+++ lacosRepeticao.php
@@ -0,0 +1,23 @@
+<?php
+  /* Estruturas de repetição
+     ou laços de repetição
+     usando a instrução While
+  */
+
+  $maximo = 10;
+  $x = 1;
+
+//   while ( $x <= $maximo ){
+//       echo "Variável X tem valor " . $x;
+//       $x++; //$x = $x + 1  
+//       echo "<br>";
+//   }
+
+  echo "<h2>Laço com a instrução FOR</h2>";
+  for($x=1;$x <= 10;$x++){
+    echo "Variável X tem valor " . $x;
+    echo "<br>";
+  }
+
+  
+
